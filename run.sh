ad3610b94d4861673999679bf67a45d3560be827
bomba-cat <bledion.morina@hotmail.com>
2024-06-24 14:15:30 +0200

    updated main yaml file and run script

--- run.sh
+++ run.sh
@@ -9,3 +9,7 @@ echo "KUZ35_:3434FEfwew*1" > ./password.txt
 
 echo Running docker-compose...
 docker-compose up -d | echo --- && echo --- && echo Error executing: docker-compose up -d
+
+echo Waiting for gitlab..
+docker wait gitlab
+docker exec -it gitlab grep 'Password:' /etc/gitlab/initial_root_password
